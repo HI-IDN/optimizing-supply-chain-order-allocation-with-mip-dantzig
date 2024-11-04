f042d830b66d2b61920b581abcac40ad11c6cbb4
vms9 <vms9@hi.is>
2024-11-04 15:40:34 +0000

    Add files via upload

--- DO_project_input.py
+++ DO_project_input.py
@@ -1,130 +1,160 @@
 import pandas as pd
+from gurobipy import Model, GRB
+import gurobipy as gb
 
 
-# OrderList = pd.read_excel("Supply chain logisitcs problem.xlsx", "OrderList")
-# FreightRates = pd.read_excel("Supply chain logisitcs problem.xlsx", "FreightRates")
-# WhCosts = pd.read_excel("Supply chain logisitcs problem.xlsx", "WhCosts")
-# WhCapacities = pd.read_excel("Supply chain logisitcs problem.xlsx", "WhCapacities")
-# ProductsPerPlant = pd.read_excel("Supply chain logisitcs problem.xlsx", "ProductsPerPlant")
-# VmiCustomers = pd.read_excel("Supply chain logisitcs problem.xlsx", "VmiCustomers")
-# PlantPorts = pd.read_excel("Supply chain logisitcs problem.xlsx", "PlantPorts")
+def input_data():
+    file_path = "Supply chain logisitcs problem.xlsx"
+    
+    
+    ####cost_per_unit################################################################################################
+    cost_per_unit = pd.read_excel(file_path, sheet_name='WhCosts', usecols=['WH', 'Cost/unit'])
+    cost_per_unit.columns = ['Plant', 'UnitCost']  # Rename columns for easier reference
+    cost_per_unit = cost_per_unit.sort_values(by='Plant').reset_index(drop=True)
+    cost_per_unit['Plant'] = cost_per_unit['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
+    cost_per_unit = cost_per_unit[cost_per_unit['Plant'] != 19]
+    #print(cost_per_unit)
+    
+    
+    ####ID_quant_per_order################################################################################################
+    ID_quant_per_order = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Order ID', 'Unit quantity'])
+    #print(ID_quant_per_order)
+    
+    
+    ####weight_per_order################################################################################################
+    weight_per_order = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Weight'])
+    #print(weight_per_order)
+    
+    
+    ####ID_per_order###############################################################################################
+    ID_per_order = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Order ID'])
+    #print(ID_per_order)
+    
+    
+    ####product_per_order###############################################################################################
+    product_per_order = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Product ID'])
+    #print(product_per_order)
+    
+    
+    ####plant_product_capabilities###############################################################################################
+    plant_product_capabilities = pd.read_excel(file_path, sheet_name='ProductsPerPlant', usecols=['Plant Code', 'Product ID'])
+    plant_product_capabilities = plant_product_capabilities.groupby('Plant Code')['Product ID'].apply(list).reset_index()
+    plant_product_capabilities = plant_product_capabilities[plant_product_capabilities['Plant Code'] != 'CND9']
+    plant_product_capabilities.reset_index(drop=True, inplace=True)
+    plant_product_capabilities.columns = ['Plant', 'ProductID']  # Rename columns for easier reference
+    plant_product_capabilities = plant_product_capabilities.sort_values(by='Plant').reset_index(drop=True)
+    plant_product_capabilities['Plant'] = plant_product_capabilities['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
+    #plant_product_capabilities = plant_product_capabilities[plant_product_capabilities['Plant'] != 19]
+    #print(plant_product_capabilities)
+    
+    
+    ####C##################################################################################################
+    customer_per_order = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Customer'])
+    #print(customer_per_order)
+    
+    
+    ####vmi_customers_per_plant################################################################################################
+    vmi_customers_per_plant = pd.read_excel(file_path, sheet_name='VmiCustomers', usecols=['Plant Code', 'Customers'])
+    
+    # create a list of plants from PLANT01 to PLANT19
+    plant_list = [f'PLANT{i:02}' for i in range(1, 20)]
+    # creat a DataFrame with all Plants and an empty 'Customers' collumn
+    all_plants = pd.DataFrame({'Plant Code': plant_list})
+    
+    vmi_customers_per_plant = vmi_customers_per_plant.groupby('Plant Code')['Customers'].apply(list).reset_index()
+    
+    #merge list from excel with list for all plants
+    vmi_customers_per_plant = all_plants.merge(vmi_customers_per_plant, on='Plant Code', how='left')
+    
+    vmi_customers_per_plant.columns = ['Plant', 'Customers']  # Rename columns for easier reference
+    vmi_customers_per_plant = vmi_customers_per_plant.sort_values(by='Plant').reset_index(drop=True)
+    vmi_customers_per_plant['Plant'] = vmi_customers_per_plant['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
+    vmi_customers_per_plant = vmi_customers_per_plant[vmi_customers_per_plant['Plant'] != 19]
+    #print(vmi_customers_per_plant)
+    
+    
+    ####daily_order_capacity_per_plant#################################################################################################
+    daily_order_capacity_per_plant = pd.read_excel(file_path, sheet_name='WhCapacities', usecols=['Plant ID', 'Daily Capacity '])
+    daily_order_capacity_per_plant.columns = ['Plant', 'Daily Capacity']  # Rename columns for easier reference
+    daily_order_capacity_per_plant = daily_order_capacity_per_plant.sort_values(by='Plant').reset_index(drop=True)
+    daily_order_capacity_per_plant['Plant'] = daily_order_capacity_per_plant['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
+    daily_order_capacity_per_plant = daily_order_capacity_per_plant[daily_order_capacity_per_plant['Plant'] != 19]
+    #print(daily_order_capacity_per_plant)
+    
+    
+    ####ports_per_plant#################################################################################################
+    ports_per_plant = pd.read_excel(file_path, sheet_name='PlantPorts', usecols=['Plant Code', 'Port'])
+    ports_per_plant.columns = ['Plant', 'Port']  # Rename columns for easier reference
+    ports_per_plant = ports_per_plant.groupby('Plant')['Port'].apply(list).reset_index()
+    
+    # Rename PORT01, PORT02, etc. to 1, 2, etc.
+    unique_ports = ports_per_plant['Port'].explode().unique()
+    port_mapping = {port: str(i + 1) for i, port in enumerate(unique_ports)}
+    ports_per_plant['Port'] = ports_per_plant['Port'].apply(lambda ports: [port_mapping[port] for port in ports])
+    
+    ports_per_plant = ports_per_plant.sort_values(by='Plant').reset_index(drop=True)
+    ports_per_plant['Plant'] = ports_per_plant['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
+    ports_per_plant = ports_per_plant[ports_per_plant['Plant'] != 19]
+    #print(ports_per_plant)
+    
+    ####costs_per_port################################################################################################
+    costs_per_port = pd.read_excel(file_path, sheet_name='FreightRates', usecols=['orig_port_cd', 'minimum cost', 'rate'])
+    costs_per_port.columns = ['Port', 'Fixed cost', 'Variable cost']  # Rename columns for easier reference
+    # Convert 'Fixed cost' and 'Variable cost' to float
+    costs_per_port['Fixed cost'] = costs_per_port['Fixed cost'].replace({'\$': '', ',': '.'}, regex=True).astype(float)
+    costs_per_port['Variable cost'] = costs_per_port['Variable cost'].replace({'\$': '', ',': '.'}, regex=True).astype(float)
+    
+    costs_per_port = costs_per_port.groupby('Port').agg({'Fixed cost': 'mean', 'Variable cost': 'mean'}).reset_index()
+    costs_per_port = costs_per_port.sort_values(by='Port').reset_index(drop=True)
+    costs_per_port['Port'] = costs_per_port['Port'].replace({'PORT': ''}, regex=True).astype(int)
+    
+    # mean value for 'Fixed cost' and 'Variable cost'
+    mean_fixed_cost = costs_per_port['Fixed cost'].mean()
+    mean_variable_cost = costs_per_port['Variable cost'].mean()
+    
+    # new row for PORT01
+    new_row = {'Port': 1, 'Fixed cost': mean_fixed_cost, 'Variable cost': mean_variable_cost}
+    
+    # insert new row in DataFrame
+    costs_per_port = pd.concat([pd.DataFrame([new_row]), costs_per_port], ignore_index=True)
+    
+    #print(costs_per_port)
+    
+    
+    
+    
+    ####DataFrame to numpy array####
+    cost_per_unit = cost_per_unit.loc[:,'UnitCost'].values
+    costs_per_port = costs_per_port.loc[:,['Fixed cost', 'Variable cost']].values
+    customer_per_order = customer_per_order.loc[:,'Customer'].values
+    daily_order_capacity_per_plant = daily_order_capacity_per_plant.loc[:,'Daily Capacity'].values
+    ID_per_order = ID_per_order.loc[:,'Order ID'].values
+    ID_quant_per_order = ID_quant_per_order.loc[:,['Order ID','Unit quantity']].values
+    plant_product_capabilities = plant_product_capabilities.loc[:,'ProductID'].values
+    ports_per_plant = ports_per_plant.loc[:,'Port'].values
+    product_per_order = product_per_order.loc[:,'Product ID'].values
+    vmi_customers_per_plant = vmi_customers_per_plant.loc[:,'Customers'].values
+    weight_per_order = weight_per_order.loc[:,'Weight'].values
 
-file_path = "Supply chain logisitcs problem.xlsx"
+    return cost_per_unit,product_per_order,ID_quant_per_order,weight_per_order,ID_per_order,plant_product_capabilities,customer_per_order,vmi_customers_per_plant,daily_order_capacity_per_plant,ports_per_plant,costs_per_port
+
+cost_per_unit,product_per_order,ID_quant_per_order,weight_per_order,ID_per_order,plant_product_capabilities,customer_per_order,vmi_customers_per_plant,daily_order_capacity_per_plant,ports_per_plant,costs_per_port = input_data()
+    
 
 
-####CPU################################################################################################
-cpu = pd.read_excel(file_path, sheet_name='WhCosts', usecols=['WH', 'Cost/unit'])
-cpu.columns = ['Plant', 'UnitCost']  # Rename columns for easier reference
-cpu = cpu.sort_values(by='Plant').reset_index(drop=True)
-cpu['Plant'] = cpu['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
-cpu = cpu[cpu['Plant'] != 19]
-print(cpu)
+# model = Model('warehouse_problem')
 
+# number_of_orders = len(ID_per_order)        # index i
+# number_of_plants = len(cost_per_unit)         # index j
+# number_of_ports = len(costs_per_port)          # index k
+# number_of_days = 1
 
-####NoI################################################################################################
-noi = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Order ID', 'Unit quantity'])
-print(noi)
-
-
-####WoO################################################################################################
-woo = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Weight'])
-print(woo)
-
-
-####OPID###############################################################################################
-opid = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Order ID'])
-print(opid)
-
-
-####PPID###############################################################################################
-ppid = pd.read_excel(file_path, sheet_name='ProductsPerPlant', usecols=['Plant Code', 'Product ID'])
-ppid = ppid.groupby('Plant Code')['Product ID'].apply(list).reset_index()
-ppid = ppid[ppid['Plant Code'] != 'CND9']
-ppid.reset_index(drop=True, inplace=True)
-ppid.columns = ['Plant', 'ProductID']  # Rename columns for easier reference
-ppid = ppid.sort_values(by='Plant').reset_index(drop=True)
-ppid['Plant'] = ppid['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
-#ppid = ppid[ppid['Plant'] != 19]
-print(ppid)
-
-
-####C##################################################################################################
-c = pd.read_excel(file_path, sheet_name='OrderList', usecols=['Customer'])
-print(c)
-
-
-####VMI################################################################################################
-vmi = pd.read_excel(file_path, sheet_name='VmiCustomers', usecols=['Plant Code', 'Customers'])
-
-# create a list of plants from PLANT01 to PLANT19
-plant_list = [f'PLANT{i:02}' for i in range(1, 20)]
-# creat a DataFrame with all Plants and an empty 'Customers' collumn
-all_plants = pd.DataFrame({'Plant Code': plant_list})
-
-vmi = vmi.groupby('Plant Code')['Customers'].apply(list).reset_index()
-
-#merge list from excel with list for all plants
-vmi = all_plants.merge(vmi, on='Plant Code', how='left')
-
-vmi.columns = ['Plant', 'Customers']  # Rename columns for easier reference
-vmi = vmi.sort_values(by='Plant').reset_index(drop=True)
-vmi['Plant'] = vmi['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
-vmi = vmi[vmi['Plant'] != 19]
-print(vmi)
-
-
-####DC#################################################################################################
-dc = pd.read_excel(file_path, sheet_name='WhCapacities', usecols=['Plant ID', 'Daily Capacity '])
-dc.columns = ['Plant', 'Daily Capacity']  # Rename columns for easier reference
-dc = dc.sort_values(by='Plant').reset_index(drop=True)
-dc['Plant'] = dc['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
-dc = dc[dc['Plant'] != 19]
-print(dc)
-
-
-####CP#################################################################################################
-cp = pd.read_excel(file_path, sheet_name='PlantPorts', usecols=['Plant Code', 'Port'])
-cp.columns = ['Plant', 'Port']  # Rename columns for easier reference
-cp = cp.groupby('Plant')['Port'].apply(list).reset_index()
-
-# Rename PORT01, PORT02, etc. to 1, 2, etc.
-unique_ports = cp['Port'].explode().unique()
-port_mapping = {port: str(i + 1) for i, port in enumerate(unique_ports)}
-cp['Port'] = cp['Port'].apply(lambda ports: [port_mapping[port] for port in ports])
-
-cp = cp.sort_values(by='Plant').reset_index(drop=True)
-cp['Plant'] = cp['Plant'].replace({'PLANT': ''}, regex=True).astype(int)
-cp = cp[cp['Plant'] != 19]
-print(cp)
-
-####cpw################################################################################################
-cpw = pd.read_excel(file_path, sheet_name='FreightRates', usecols=['orig_port_cd', 'minimum cost', 'rate'])
-cpw.columns = ['Port', 'Fixed cost', 'Variable cost']  # Rename columns for easier reference
-# Convert 'Fixed cost' and 'Variable cost' to float
-cpw['Fixed cost'] = cpw['Fixed cost'].replace({'\$': '', ',': '.'}, regex=True).astype(float)
-cpw['Variable cost'] = cpw['Variable cost'].replace({'\$': '', ',': '.'}, regex=True).astype(float)
-
-cpw = cpw.groupby('Port').agg({'Fixed cost': 'mean', 'Variable cost': 'mean'}).reset_index()
-cpw = cpw.sort_values(by='Port').reset_index(drop=True)
-cpw['Port'] = cpw['Port'].replace({'PORT': ''}, regex=True).astype(int)
-
-# mean value for 'Fixed cost' and 'Variable cost'
-mean_fixed_cost = cpw['Fixed cost'].mean()
-mean_variable_cost = cpw['Variable cost'].mean()
-
-# new row for PORT01
-new_row = {'Port': 1, 'Fixed cost': mean_fixed_cost, 'Variable cost': mean_variable_cost}
-
-# insert new row in DataFrame
-cpw = pd.concat([pd.DataFrame([new_row]), cpw], ignore_index=True)
-
-print(cpw)
-
-
-
-
-
-
-
-
-
+# plant_alloc = model.addVars(number_of_orders, number_of_plants, number_of_days, vtype=GRB.BINARY,name="Plant Allocation")        # number of plant j producing order i
+# port_alloc = model.addVars(number_of_orders, number_of_ports, number_of_days, vtype=GRB.BINARY,name="Port Allocation")        # number of port k shipping order i
+    
+# def objective_function(cost_per_unit,costs_per_port,ID_quant_per_order,plant_alloc,port_alloc):
+#     gb.quicksum(cost_per_unit[])
+    
+    
+    
+    
